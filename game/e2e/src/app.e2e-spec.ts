a591836b8322bcc5202c3dbd41cde38923e0b6d4
Louis-Guiniot <louis99679@gmail.com>
2021-03-17 18:34:01 +0100

    progetto ricreato + home

--- game/e2e/src/app.e2e-spec.ts
+++ game/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('jac-game app is running!');
+    expect(await page.getTitleText()).toEqual('game app is running!');
   });
 
   afterEach(async () => {
--- game/src/app/app.component.spec.ts
+++ game/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jac-game'`, () => {
+  it(`should have as title 'game'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jac-game');
+    expect(app.title).toEqual('game');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('jac-game app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('game app is running!');
   });
 });
