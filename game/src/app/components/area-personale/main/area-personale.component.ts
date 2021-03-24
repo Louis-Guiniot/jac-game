a71a10fb8a848af21ecb345f1adddcfd9098e10a
Louis-Guiniot <61980982+Louis-Guiniot@users.noreply.github.com>
2021-03-24 08:56:42 +0100

    area personale form grafica apposto. non collegata a niente

--- game/src/app/components/area-personale/main/area-personale.component.ts
+++ game/src/app/components/area-personale/main/area-personale.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-area-personale',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AreaPersonaleComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  areaPersonaleForm:FormGroup
+
+  mesiSelect = [
+    {label:'Gennaio'},
+    {label:'Febbraio'}, 
+    {label:'Marzo'},
+    {label:'Aprile'},
+    {label:'Maggio'}, 
+    {label:'Giugno'},
+    {label:'Luglio'},
+    {label:'Agosto'},
+    {label:'Settembre'},
+    {label:'Ottombre'},
+    {label:'Novembre'},
+    {label:'Dicembre'}
+  ]
+
+  giorniSelect = []
+  anniSelect = []
 
   ngOnInit(): void {
+
+    for(let i = 1; i<=31; i++){
+      this.giorniSelect.push({label:i})
+    }
+
+    for(let i = 1970; i<=2021; i++){
+      this.anniSelect.push({label:i})
+    }
+
+    this.areaPersonaleForm = this.fb.group({
+      nome: [''],
+      cognome: [''],
+      email: [''],
+      password: ['']
+    })
+
+    console.log(this.mesiSelect)
   }
 
 }
