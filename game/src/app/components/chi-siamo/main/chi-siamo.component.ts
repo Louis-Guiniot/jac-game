bc659015e1d8550fd0bc2b1e5b35c9fe6805ea5e
Louis-Guiniot <61980982+Louis-Guiniot@users.noreply.github.com>
2021-03-27 10:18:17 +0100

    fix

--- game/src/app/components/chi-siamo/main/chi-siamo.component.ts
+++ game/src/app/components/chi-siamo/main/chi-siamo.component.ts
@@ -9,6 +9,12 @@ export class ChiSiamoComponent implements OnInit {
 
   constructor() { }
 
+  corso1 = [
+    {desc:'Project work',hour:'30'},{desc:'Incontri con Realt&agrave; aziendali',hour:'15'},{desc:'Programmazione orientata agli oggetti',hour:'90'},{desc:'Programmazione web I',hour:'90'},
+    {desc:'Project work',hour:'30'},{desc:'Programmazione back-end e PHP',hour:'45'},{desc:'Web app e User Experience',hour:'90'},{desc:'Programmazione SQL del database',hour:'45'},
+    {desc:'English Coaching I',hour:'60'},{desc:'Business system and approach to work I',hour:'30'},{desc:'Fogli di lavoro',hour:'30'},{desc:'Estetica del lavoro I',hour:'45'}  
+  ]
+
   ngOnInit(): void {
   }
 
